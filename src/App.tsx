64355ddee8d7cf36b7cd379ef8de5683bb1b3016
Mario García <mariogrdg02@outlook.com>
2025-10-23 18:24:52 +0200

    Cambios en experiencia laboral

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <header className="flex flex-col items-center justify-center h-screen gap-12 px-12 py-12 bg-gray-300 md:justify-between lg:py-48 lg:px-48 md:flex-row">
+      <section className="flex flex-col justify-center h-screen gap-12 px-12 py-12 bg-gray-300 md:items-center md:justify-between lg:py-48 lg:px-48 md:flex-row">
         <div>
           <h1 className="text-4xl font-bold">Mario García Relaño</h1>
         <h2 className="mt-2 text-2xl text-gray-700">Programador Full-Stack</h2>
@@ -13,15 +13,16 @@ function App() {
           <p>likedin</p>
           <p>correo</p>
         </div>
-      </header>
-      <section className="items-center px-12 py-12 lg:py-48 lg:px-48 lg:items-start">
+      </section>
+      <section className="items-center px-12 py-12 lg:py-24 lg:px-48 lg:items-start">
         <h2 className="mb-12 text-3xl font-bold">Experiencia laboral</h2>
-        <ol>
+        <ol className="relative pl-10 border-l-2 border-gray-300">
           <li>
             <div className="grid pb-12 md:grid-cols-5">
-              <div className="pb-12 md:col-span-2">
-                <h3>Diseñador Web</h3>
-                <h4>Future Space</h4>
+              <span className="absolute -left-[1px] text-5xl -translate-x-1/2 rounded-full">•</span>
+              <div className="pb-8 md:col-span-2">
+                <h3 className="text-2xl font-bold">Diseñador Web</h3>
+                <h4 className="text-xl font-bold">Future Space</h4>
                 <p><time>Septiembre 2022</time> a <time>Junio 2023</time></p>
               </div>
               <div className="md:col-span-3">
@@ -31,8 +32,8 @@ function App() {
           </li>
         </ol>
       </section>
-      <section>
-        <h2>Proyectos</h2>
+      <section className="items-center px-12 py-12 lg:py-24 lg:px-48 lg:items-start">
+        <h2 className="mb-12 text-3xl font-bold">Proyectos</h2>
         <p>HOLA</p>
       </section>
       <section>
